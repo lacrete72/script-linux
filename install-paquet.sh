5751aaf56aebf4cff977625ba2fb0c4125b7307d
lacrete72 <lacrete72@gmail.com>
2025-09-18 10:27:32 +0200

    Update install-paquet.sh

--- install-paquet.sh
+++ install-paquet.sh
@@ -1,1 +1,11 @@
-
+read -p "Entrez le nom du paquet à installer : " paquet
+            if dpkg -s "$paquet" &> /dev/null; then
+                echo "Le paquet '$paquet' est déjà installé."
+            else
+                apt install -y "$paquet"
+                if [[ $? -eq 0 ]]; then
+                    echo "Paquet '$paquet' installé avec succès."
+                else
+                    echo "Erreur lors de l'installation du paquet."
+                fi
+            fi
