1365712989bff1fb6530a1155651670bc3231b32
lacrete72 <lacrete72@gmail.com>
2025-09-18 10:23:15 +0200

    Create menu.sh

---
+++ menu.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+while true; do
+    clear
+    echo "===== MENU ====="
+    echo "1) Menu utilisateur"
+    echo "2) Menu paquet"
+    echo "3) Quitter"
+    echo "================"
+    read -p "Choisissez une option [1-3] : " choix
+
+    case $choix in
+        1)
+            ./menu-utilisateur.sh
+            read -p "Appuyez sur Entrée pour continuer..." ;;
+        2)
+            ./menu-paquet.sh
+            read -p "Appuyez sur Entrée pour continuer..." ;;
+        3)
+            echo "Au revoir !"
+            exit 0 ;;
+        *)
+            echo "Option invalide."
+            read -p "Appuyez sur Entrée pour continuer..." ;;
+    esac
+done
