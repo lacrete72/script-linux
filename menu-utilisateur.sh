37f82466cb330ff4a3e2bc5bbf5ad082a5344345
lacrete72 <lacrete72@gmail.com>
2025-09-18 11:18:53 +0200

    Update menu-utilisateur.sh

--- menu-utilisateur.sh
+++ menu-utilisateur.sh
@@ -15,7 +15,7 @@ while true; do
 
     case $choix in
         1)
-            ./ajout-utilisateur.sh
+            ajout-utilisateur.sh
             read -p "Appuyez sur Entrée pour continuer..." ;;
         2)
             echo "Liste des utilisateurs :"
