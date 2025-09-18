af9b1252bd62137c495f749231311d20489a00c9
lacrete72 <lacrete72@gmail.com>
2025-09-18 15:14:59 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Définir les couleurs avec des variables
+    RED='\033[0;31m'
+    GREEN='\033[0;32m'
+    YELLOW='\033[0;33m'
+    NC='\033[0m'  # No Color pour réinitialiser la couleur
 source ./menu.sh
 
 menu
