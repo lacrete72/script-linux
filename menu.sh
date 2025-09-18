7ca1f467d3892a60b192472ec79a9a42d73cbe85
lacrete72 <lacrete72@gmail.com>
2025-09-18 15:19:35 +0200

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -5,6 +5,7 @@ menu(){
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
+NC='\033[0m'  # No color, pour réinitialiser
 
 # ajout des script
 source ./menu_paquet.sh
@@ -16,8 +17,8 @@ while true; do
     echo -e "${GREEN}1) Menu utilisateur"
     echo -e "${GREEN}2) Menu paquet"
     echo -e "${GREEN}3) Quitter"
-    echo -e "${YELLOW}================"
-    read -p -e "${GREEN}Choisissez une option [1-3] : " choix
+    echo -e "${YELLOW}================${NC}"
+    read -p "Choisissez une option [1-3] : " choix
 
     case $choix in
         1)
