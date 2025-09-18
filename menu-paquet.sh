e056226c8edea5b2dbc2b75171a30ddb569e5e4e
lacrete72 <lacrete72@gmail.com>
2025-09-18 10:27:13 +0200

    Update menu-paquet.sh

--- menu-paquet.sh
+++ menu-paquet.sh
@@ -19,17 +19,7 @@ while true; do
 
     case $choix in
         1)
-            read -p "Entrez le nom du paquet à installer : " paquet
-            if dpkg -s "$paquet" &> /dev/null; then
-                echo "Le paquet '$paquet' est déjà installé."
-            else
-                apt install -y "$paquet"
-                if [[ $? -eq 0 ]]; then
-                    echo "Paquet '$paquet' installé avec succès."
-                else
-                    echo "Erreur lors de l'installation du paquet."
-                fi
-            fi
+            install-paquet.sh
             read -p "Appuyez sur Entrée pour continuer..." ;;
         2)
             read -p "Entrez le nom du paquet à vérifier : " paquet
